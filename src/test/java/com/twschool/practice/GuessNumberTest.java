5e9ab6b8e977fc14b2248b33fa0fd833b6809e38
humenglin <humenglin@zyb.com.cn>
2020-06-16 20:59:48 +0800

    [test]:编写所有测试案例的空方法

---
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -0,0 +1,35 @@
+package com.twschool.practice;
+
+import org.junit.Test;
+
+public class GuessNumberTest {
+    @Test
+    public void should_return_4A0B_when_answer_given_user_answer_is_same_with_game_answer() {
+
+    }
+
+    @Test
+    public void should_return_2A2B_when_answer_given_user_answer_and_game_answer_has_four_same_words_but_partly_same_positions() {
+
+    }
+
+    @Test
+    public void should_return_1A2B_when_answer_given_user_answer_and_game_answer_has_partly_same_words_and_partly_same_positions() {
+
+    }
+
+    @Test
+    public void should_return_0A4B_when_answer_given_user_answer_and_game_answer_has_four_same_words_but_different_positions() {
+
+    }
+
+    @Test
+    public void should_return_0A3B_when_answer_given_user_answer_and_game_answer_has_partly_same_words_and_partly_same_positions() {
+
+    }
+
+    @Test
+    public void should_return_0A0B_when_answer_given_user_answer_and_game_answer_has_different_words_and_different_positions() {
+
+    }
+}
