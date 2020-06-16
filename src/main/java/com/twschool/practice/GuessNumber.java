fac6cf4c24048d286ef619352c9c6acc977d5cdc
humenglin <humenglin@zyb.com.cn>
2020-06-16 21:15:23 +0800

    [feat]:当位置和数字全对时返回4A0B

---
+++ src/main/java/com/twschool/practice/GuessNumber.java
@@ -0,0 +1,13 @@
+package com.twschool.practice;
+
+public class GuessNumber {
+    private String gameAnswer;
+
+    public GuessNumber(String gameAnswer) {
+        this.gameAnswer = gameAnswer;
+    }
+
+    public String answer(String userAnswer) {
+        return "4A0B";
+    }
+}
--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -1,11 +1,25 @@
 package com.twschool.practice;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class GuessNumberTest {
+    private GuessNumber guessNumber;
+
+    @Before
+    public void setUp() {
+        guessNumber = new GuessNumber("1 2 3 4");
+    }
+
     @Test
     public void should_return_4A0B_when_answer_given_user_answer_is_same_with_game_answer() {
+        String userAnswer = "1 2 3 4";
 
+        String gameResult = guessNumber.answer(userAnswer);
+
+        assertEquals("4A0B", gameResult);
     }
 
     @Test
